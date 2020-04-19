767d658c122ca63839d9ab61d7ce3cecd61ccc08
Carlos Martínez Medina <carlos.martinez.medina1992@gmail.com>
2020-04-19 20:21:33 +0200

    Removed unnecessary conditional.

--- scripts/laravel-install.sh
+++ scripts/laravel-install.sh
@@ -2,10 +2,8 @@
 
 applicationName="$1";
 
-if [ ! -f /home/vagrant/.laravel-installed ]; then
-    cd /home/vagrant/"$applicationName" || exit;
+cd /home/vagrant/"$applicationName" || exit;
 
-    composer create-project --prefer-dist laravel/laravel code;
+composer create-project --prefer-dist laravel/laravel code;
 
-    echo "$applicationName" > /home/vagrant/.laravel-installed;
-fi;
+echo "$applicationName" > /home/vagrant/.laravel-installed;
